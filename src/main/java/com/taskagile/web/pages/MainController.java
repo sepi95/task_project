c74cf837f74017a9e6a1e8c408e08815afe9104d
jiyoung <yoni09moo@gmail.com>
2021-01-04 00:30:59 +0900

    fix: modify missing code

--- src/main/java/com/taskagile/web/pages/MainController.java
+++ src/main/java/com/taskagile/web/pages/MainController.java
@@ -1,9 +1,11 @@
+package com.taskagile.web.pages;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-  @GetMapping(value={"/", "/login"})
+  @GetMapping(value = { "/", "/login" })
   public String entry() {
     return "index";
   }
